de55250fed7fb1d14844a29dac8a99a182b8ccbb
suliman2005elkorti@gmail.com <suliman2005elkorti@gmail.com>
2026-05-10 17:46:49 +0400

    changes for login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,3 +1,28 @@
 <template>
     <PageTitle>Login</PageTitle>
+    <div>
+        <h1>login</h1>
+        <input v-model="email" type="email" placeholder="Email" />
+        <input v-model="password" type="password" placeholder="Password" />
+        <button @click="login">Login</button>
+        <p style="color:red">{{ error }}</p>
+    </div>
 </template>
+
+<script setup>
+    const email = ref('')
+    const password = ref('')
+    const error = ref('')
+    const isLoggedIn = useState('isLoggedIn', () => false)
+
+    const login = async () => {
+        console.log(email.value)
+        console.log(password.value)
+        if (email.value === 'suliman2005elkorti@gmail.com' && password.value === '123456') {
+            isLoggedIn.value = true
+            await navigateTo('/dashboard')
+        } else {
+            error.value = 'Wrong email or password'
+    }
+}
+</script>
